33c008285597964c40581e761ccdeaf86bcfa954
Unction-F <2148765298@qq.com>
2023-01-21 15:55:25 +0800

    fix download but unfinished

--- LinspirerAppStoreEnumerator.NET/Program.cs
+++ LinspirerAppStoreEnumerator.NET/Program.cs
@@ -14,29 +14,39 @@ namespace LinspirerAppStoreEnumerator.NET
 
         static void EnumerateApp(Object AppID)
         {
-            var id = (int)AppID;
-            var download = new Task(() =>
+            var id = 10093;//(int)AppID;
+            var download = new Task( () =>
                 {
                     var mac = "4a";
                     var email = "null";
                     var url = $"https://cloud.linspirer.com:883/download.php?email={email}&appid={id}&swdid={mac}&version={new Random(DateTime.Now.Millisecond).Next(1, 9000000)}";
-                    var save = $"./apks/{id}.apk";
+                    var save = @".\"+id.ToString()+".apk";
                     var http = new HttpClient();
                     var request = new HttpRequestMessage(HttpMethod.Get, url);
                     request.Headers.Add("Range", "bytes=0-0");
                     request.Headers.Add("user-agent", "okhttp/3.10.0");
-                    var response = http.Send(request);
 
                     Log.WriteLog(Log.LogLevel.Info, $"App {id} downloading...");
+
+                    var response = http.Send(request);
+
+                    if(response.StatusCode!=System.Net.HttpStatusCode.Redirect)
+                    {
+                        Log.WriteLog(Log.LogLevel.Info, $"App {id} do not exist!");
+                        return;
+                    }
+
+                    request = new HttpRequestMessage(HttpMethod.Get, response.Headers.Location);
+                    response = http.Send(request);
 
                     try
                     {
                         response.EnsureSuccessStatusCode();
-                        using (var fs = File.Open(save, FileMode.Create))
+                        using (var fs = File.Open(save, FileMode.OpenOrCreate))
                         {
                             using (var ms = response.Content.ReadAsStream())
                             {
-                                ms.CopyToAsync(fs);
+                                ms.CopyTo(fs);
                             }
                         }
                     }
@@ -48,21 +58,20 @@ namespace LinspirerAppStoreEnumerator.NET
 
                     Log.WriteLog(Log.LogLevel.Info, $"App {id} downloaded!");
                 });
+
             Log.WriteLog(Log.LogLevel.Info, $"App {id} started.");
 
             if (Args.Object.IsSaveApk)
             {
                 download.Start();
+                download.Wait();
             }
 
+            //Log.WriteLog(Log.LogLevel.Error, $"App ID: {id} occurred error!");
 
-            Log.WriteLog(Log.LogLevel.Error, $"App ID: {id} occurred error!");
-
-
-            download.Wait();
 
             Log.WriteLog(Log.LogLevel.Info, $"App ID: {id} done.");
-
+            Thread.Sleep(500);
         }
 
         public static int Main(string[] RawArgs)
