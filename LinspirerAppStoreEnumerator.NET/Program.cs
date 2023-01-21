354ccd8d85f015217afb5e19b843c42e7585bea9
Unction-F <2148765298@qq.com>
2023-01-21 10:07:09 +0800

    添加项目文件。

---
+++ LinspirerAppStoreEnumerator.NET/Program.cs
@@ -0,0 +1,61 @@
+﻿using Fclp;
+using System;
+using static System.Net.Mime.MediaTypeNames;
+
+namespace LinspirerAppStoreEnumerator.NET
+{
+    public class ApplicationArguments
+    {
+        public int StartId { get; set; }
+        public int EndId { get; set; }
+        public int ThreadNum { get; set; }
+        public bool IsSaveApk { get; set; }
+        public bool IsRecalled { get; set; }
+    }
+
+    public class App
+    {
+        public FluentCommandLineParser<ApplicationArguments> CommandLineArgsProcess(string[] args)
+        {
+            var p = new FluentCommandLineParser<ApplicationArguments>();
+
+            p.SetupHelp("?", "help")
+                .Callback(()=>Console.WriteLine("ehh"));
+
+            p.Setup(arg => arg.StartId)
+                .As('s', "startid")
+                .Required();
+
+            p.Setup(arg => arg.StartId)
+                .As('e', "endid")
+                .Required();
+
+            p.Setup(arg => arg.ThreadNum)
+                .As('t', "thread")
+                .Required();
+
+            p.Setup(arg => arg.IsSaveApk)
+                .As('s', "save")
+                .SetDefault(true);
+
+            p.Setup(arg => arg.IsRecalled)
+                .As('r', "recallled")
+                .SetDefault(true);
+
+            p.Parse(args);
+
+            return p;
+        }
+
+        public int Main(string[] RawArgs)
+        {
+            var args = CommandLineArgsProcess(RawArgs);
+
+
+            return 0;
+        }
+    }
+
+
+
+}
